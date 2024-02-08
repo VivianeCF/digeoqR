aba8a940381a37514b55b39b53557a72752fb64a
Viviane Carillo Ferrari <vcarilloferrari@gmail.com>
2024-02-08 11:08:20 -0300

    arruma para criar diretório principal

--- R/estrutura_diretorio.R
+++ R/estrutura_diretorio.R
@@ -1,39 +1,43 @@
 
 #' Cria diretórios e subdiretórios para o processamento de dados
 #'
+#' @param path diretório do projeto
+#'
 #' @return Cria a estrutura dos diretórios onde devem ser lidos os arquivos de
 #'   entrada e salvos os arquivos de saída
 #' @export
 #'
 #' @examples
 #'
-estrutura_diretorio <- function() {
+estrutura_diretorio <- function(path) {
+  dir.create(path)
   # Diretório de entrada de dados
-  dir.create('inputs')
-  dir.create('outputs')
+  dir.create(paste0(path,'inputs'))
+  dir.create(paste0(path,'outputs'))
   # Dados mineralométricos
-  dir.create('inputs/mineral')
-  dir.create('inputs/mineral/B')
-  dir.create('inputs/mineral/L')
-  dir.create('inputs/mineral/R')
-  dir.create('inputs/mineral/S')
+  dir.create(paste0(path,'inputs/mineral'))
+  dir.create(paste0(path,'inputs/mineral/B'))
+  dir.create(paste0(path,'inputs/mineral/L'))
+  dir.create(paste0(path,'inputs/mineral/R'))
+  dir.create(paste0(path,'inputs/mineral/S'))
 
   # Dados Químicos
-  dir.create('inputs/quimica')
-  dir.create('inputs/quimica/B')
-  dir.create('inputs/quimica/L')
-  dir.create('inputs/quimica/R')
-  dir.create('inputs/quimica/S')
+  dir.create(paste0(path,'inputs/quimica'))
+  dir.create(paste0(path,'inputs/quimica/B'))
+  dir.create(paste0(path,'inputs/quimica/L'))
+  dir.create(paste0(path,'inputs/quimica/R'))
+  dir.create(paste0(path,'inputs/quimica/S'))
 
   # Dados de Campo
-  dir.create('inputs/campo')
+  dir.create(paste0(path,'inputs/campo'))
 
   # Dados de Projetos do Rgeo
-  dir.create('inputs/projetos')
+  dir.create(paste0(path,'inputs/projetos'))
 
   # Dados de Imagens (SRTM)
-  dir.create('inputs/imagens')
+  dir.create(paste0(path,'inputs/imagens'))
 
   # Dados Diversos
-  dir.create('inputs/diversos')
-}
+  dir.create(paste0(path,'inputs/diversos'))
+
+  }

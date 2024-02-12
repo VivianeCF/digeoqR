e45601ae9030642ca467caa90105c8fc435747ff
Viviane CF <vcarilloferrari@gmail.com>
2024-02-12 19:39:06 -0300

    dois tipos de legenda

--- R/intersecta_bacias.R
+++ R/intersecta_bacias.R
@@ -51,18 +51,25 @@ intersecta_bacias <- function(dir_campo = "inputs/campo/",
   spy_grid$Area_bacia <-
     round(sf::st_area(spy_grid, byid = TRUE) / 1000000, 3)
   # spy_grid@data <- spy_grid@data[,1:8]
-  spy_poly = sf::read_sf(paste0(dir_campo, feicao, ".shp"))
+  spy_poly = sf::read_sf(file_shp)
   spy_poly <- sf::st_transform(spy_poly,
                                paste0(
                                  "+proj=utm +zone=",
                                  zone,
                                  " +south +datum=WGS84 +units=m +no_defs"
                                ))
+  colnames(spy_poly)
+if(tipo_leg == 2){
+  spy_poly <- spy_poly %>%
+    dplyr::group_by(RANGE) %>%
+    dplyr::summarize()
+}
+
   legenda <- prepara_legenda(file_shp, file_xml )
   codlito <- legenda[[tipo_leg]]
 
   mydata <- sf::read_sf(paste0(dir_campo, estacoes, ".shp"))
-
+  colnames(spy_grid)
   spy_grid <-
     dplyr::left_join(spy_grid , data.frame(mydata), by = "VALUE")
   area_lito <-
@@ -83,6 +90,13 @@ intersecta_bacias <- function(dir_campo = "inputs/campo/",
   area_lito$Pct_area <-
     as.numeric(round(area_lito$Area_lito / area_lito$Area_bacia * 100, 2))
   df <- as.data.frame(area_lito)
+
+  if(tipo_leg == 1){
+    chave <- "SIGLA"
+  }else{
+    chave <- "RANGE"
+  }
+
   select_campos <- c(
     "VALUE",
     "Area_sqm",
@@ -99,8 +113,7 @@ intersecta_bacias <- function(dir_campo = "inputs/campo/",
     "LOTE",
     "REQUERENTE",
     "DATA",
-    "SIGLA",
-    "NOME",
+    chave,
     "Area_lito",
     "Pct_area"
   )
@@ -141,73 +154,17 @@ intersecta_bacias <- function(dir_campo = "inputs/campo/",
   # do.call("rbind", lst)
   ##ID <- seq(1:nrow(spy_grid@data))
   dc <- cbind(do.call("rbind", lst), spy_grid)
-
+colnames(dc)[1] <- "SIGLA"
 
     dc <-
-    dplyr::right_join(codlito[, c("SIGLA", "RGB", "Geo_cod")], dc,  by =  "SIGLA")
-  # colnames(dc)
-  select_campos2 <-
-    c(
-      "SIGLA",
-      "RGB" ,
-      "Geo_cod"  ,
-      "OBJECTID"  ,
-      "ID_UNIDADE",
-      "HIERARQUIA" ,
-      "NOME"  ,
-      "AMBIENTE_T",
-      "SUB_AMBIEN",
-      "SIGLA_PAI" ,
-      "NOME_PAI"  ,
-      "LEGENDA"   ,
-      "ESCALA"  ,
-      "MAPA" ,
-      "LITOTIPOS" ,
-      "RANGE"   ,
-      "IDADE_MIN" ,
-      "IDADE_MAX" ,
-      "EON_MIN"  ,
-      "EON_MAX"   ,
-      "ERA_MIN"  ,
-      "ERA_MAX"   ,
-      "SISTEMA_MI",
-      "SISTEMA_MA",
-      "EPOCA_MIN" ,
-      "EPOCA_MAX" ,
-      "SIGLAS_HIS",
-      "GRUPO"  ,
-      "SHAPE.AREA" ,
-      "SHAPE.LEN" ,
-      "VALUE" ,
-      "Area_sqm"  ,
-      "Area_bacia",
-      "N_LAB"  ,
-      "NUM_CAMPO",
-      "LONG_DEC",
-      "LAT_DEC"  ,
-      "UTM_LESTE" ,
-      "UTM_NORTE" ,
-      "MC",
-      "PROJETO"   ,
-      "CLASSE"    ,
-      "LOTE" ,
-      "REQUERENTE",
-      "DATA"
-    )
-  dc$Area_bacia <- as.numeric(dc$Area_bacia)
-  write.csv2(
-    dc[, select_campos2],
-    paste0(dir_out, "mylitho_completa.csv"),
-    row.names = FALSE,
-    fileEncoding = "latin1"
-  )
- out[[2]] <- dc[, select_campos2]
+    dplyr::right_join(codlito[, c("SIGLA", "RGB", "Geo_cod")], dc,  by = "SIGLA")
+
   mylitho <- dc[, c("VALUE", "Area_bacia", "Geo_cod")]
   mylitho$Area_bacia <- as.numeric(mylitho$Area_bacia)
   write.csv2(mylitho,
              paste0(dir_out,"mylitho.csv"),
              row.names = FALSE,
              fileEncoding = "latin1")
-  out[[3]] <- mylitho
+  out[[2]] <- mylitho
   return(out)
 }

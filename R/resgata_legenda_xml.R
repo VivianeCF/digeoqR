65c711d9a8e920f870012c8d3624d3ee7b54be87
Viviane CF <vcarilloferrari@gmail.com>
2024-02-17 15:00:03 -0300

    arruma argumentos

--- R/resgata_legenda_xml.R
+++ R/resgata_legenda_xml.R
@@ -8,24 +8,24 @@
 #' @param indice Índice dos registros da legenda xml
 #' @param value_cor Nome da variável com as cores
 #' @param find2 Nó de busca para os nomes das siglas
+#' @param dir_xml Diretório do arquivo xml
+#' @param nome_xml Nome do arquivo xml
 #' @param value_name Nome da variável dos nomes das siglas
-#' @param value_sub Caracteres para substituir no nome das siglas
-#' @param file_xml Endereço do arquivo de estilo xml
 #'
 #' @return
 #' @export
 #'
 #' @examples
 resgata_legenda_xml <-
-  function(file_xml = "inputs/campo/geologia.xml" ,
+  function(dir_xml = "inputs/campo/",
+           nome_xml = "geologia" ,
            find1 = "//symbol/layer" ,
            indice = "id",
            value_cor = "value",
            find2 = "//symbol" ,
-           value_name = "name",
-           value_sub = "geologia ") {
+           value_name = "name") {
     # Lê arquivo xml de estilo do QGIS
-    Lines <- xml2::read_xml(file_xml)
+    Lines <- xml2::read_xml(paste0(dir_xml, nome_xml, ".xml"))
 
     # Extrai o valores de id do nó symbol/layer
     recs <- xml2::xml_find_all(Lines, find1)
@@ -52,7 +52,7 @@ resgata_legenda_xml <-
         trimws(xml2::xml_attr(xml2::xml_child(recs2[[i]], 2), indice))
     }
     nome <-
-      gsub(value_sub,
+      gsub(paste0(nome_xml, " "),
            "",
            nome,
            fixed = TRUE)

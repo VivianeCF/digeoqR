351f0c96a744a4db486f0dcfbfd21b33fcaeeff5
Viviane CF <vcarilloferrari@gmail.com>
2024-02-18 11:19:07 -0300

    corrige chave do join, lida com mensagens e argumentos

--- R/recorta_feicao_area.R
+++ R/recorta_feicao_area.R
@@ -13,21 +13,37 @@
 #' @export
 #'
 #' @examples
-recorta_feicao_area <- function(dir_in = "inputs/campo/", dir_out = "outputs/",
+recorta_feicao_area <- function(dir_in = "inputs/campo/",
+                                dir_out = "outputs/",
                                 area = "area_srtm",
                                 feicao_in = "geologia_br",
-                                feicao_out = "geologia_area"){
+                                feicao_out = "geologia_area",
+                                chave = "SIGLA"){
+
+    suppressMessages({sf::sf_use_s2(FALSE)})
 
     area_sf <- sf::read_sf(paste0(dir_in, area, ".shp"))
     feicao_sf <- sf::read_sf(paste0(dir_in, feicao_in, ".shp"))
+
     area_sf <- sf::st_transform(area_sf, crs = 4326)
     feicao_sf <- sf::st_transform(feicao_sf, crs = 4326)
 
 
- crop_feicao <- suppressMessages({suppressWarnings({sf::st_crop(feicao_sf, area_sf)})})
- nc_dissolve <- crop_feicao %>% group_by(SIGLA) %>% suppressMessages({summarize()})
- # plot(nc_dissolve)
- feicao_edit <- suppressWarnings({suppressMessages({sf::st_join(nc_dissolve,crop_feicao, by = "SIGLA")})})
- sf::write_sf(feicao_edit, paste0(dir_out, feicao_out, ".shp"), delete_layer = TRUE)
- return( feicao_edit)
+    crop_feicao <-
+      suppressMessages({suppressWarnings({sf::st_crop(feicao_sf, area_sf)})})
+
+    nc_dissolve <-
+      suppressMessages({crop_feicao %>% dplyr::group_by(get(chave)) %>%
+      dplyr::summarize()})
+
+    # plot(nc_dissolve)
+
+    feicao_edit <-
+      suppressWarnings({suppressMessages({sf::st_join(crop_feicao, nc_dissolve,
+                                                      by = chave)})})
+    feicao_edit <- feicao_edit[,-ncol(feicao_edit)]
+    sf::write_sf(feicao_edit, paste0(dir_out, feicao_out, ".shp"),
+                  delete_layer = TRUE)
+
+    return( feicao_edit)
  }

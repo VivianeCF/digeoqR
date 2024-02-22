5637df796ab285fc1d4dd5c5a48df45ef63dbf47
Viviane CF <vcarilloferrari@gmail.com>
2024-02-22 19:44:06 -0300

    funçoes para mapa tif

---
+++ R/log_class_bxp.R
@@ -0,0 +1,4 @@
+log_class_bxp <- function (x) {
+  cutter(x, zcut_bxp(x))
+  
+}
---
+++ R/zcut_bxp.R
@@ -0,0 +1,15 @@
+zcut_bxp <- function (x) {
+  z <- log10(x)
+  q <-quantile(z, probs = c(0.25, 0.75), na.rm = TRUE)
+  zcut <- numeric(8)
+  hw <- q[2] - q[1]
+  zcut[1] <- q[1] - 4.5 * hw
+  zcut[2] <- q[1] - 3 * hw
+  zcut[3] <- q[1] - 1.5 * hw
+  zcut[4] <- q[1]
+  zcut[5] <- q[2]
+  zcut[6] <- q[2] + 1.5 * hw
+  zcut[7] <- q[2] + 3 * hw
+  zcut[8] <- q[2] + 4.5 * hw
+  10^zcut
+}

0cc90e760c68c0960fdac336995573e6cf40537f
Viviane Carillo Ferrari <vcarilloferrari@gmail.com>
2024-04-26 12:32:12 -0300

    corrige rbind

--- R/gera_estacoes.R
+++ R/gera_estacoes.R
@@ -237,6 +237,7 @@ gera_estacoes <-
           sf::st_intersection(pontos_area, area_urbana)
         })
       })
+    pontos_area_remover1 <- pontos_area_remover1[, colnames(pontos_area_remover2)]
     if ((nrow(pontos_area_remover2) + nrow(pontos_area_remover1)) > 0) {
       pontos_remover <- rbind(pontos_area_remover1, pontos_area_remover2)
       pontos_area <-

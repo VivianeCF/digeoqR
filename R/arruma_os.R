7dd9069a3de9ab1fb1bf38667b66852a34280620
Viviane Carillo Ferrari <vcarilloferrari@gmail.com>
2024-05-23 14:02:18 -0300

    arruma leitura das os

--- R/arruma_os.R
+++ R/arruma_os.R
@@ -7,13 +7,9 @@
 #' @export
 #'
 #' @examples
-arruma_os <- function(dir_os = "Data/PG2024/Rocha/os_solicitada/",  
-                      doc_lab = "sgs-geosol_metodos-analiticos-03_2024.xlsx" ){
+arruma_os <- function(dir_os = "Data/PG2024/Rocha/os_solicitada/"){
 
-# dir_os <- "Data/PG2024/Rocha/OS_RA/"
 list_os <- paste0(dir_os, list.files(dir_os, pattern = "*.xlsx"))
-df_os = list()
-acess <- readxl::read_xlsx(doc_lab)
 os <- list()
 for (i in 1:length(list_os)) {
 ra_am <- readxl::read_xlsx(list_os[i],
@@ -22,24 +18,26 @@ ra_am <- readxl::read_xlsx(list_os[i],
 ra_gerais <- readxl::read_xlsx(list_os[i],
                                sheet = "DadosGerais")
 projeto <- as.character(ra_gerais[39, 10])
+variaveis_am <- as.character(ra_am[4,])
+variaveis_am[c(1:10,76:77,78)] <- c("ID", "Longitude", "Latitude", "N_CAMPO", "Litotipo", "Lote",
+                                    "N_LAB", "CLASSE_AM", "Matriz", "Peso_gr", "Mat_ref", "Massa_min", "Observações")
 linha_final <- 5 + as.numeric(ra_gerais[38, 10])
-ra_am_sel <- ra_am[5:linha_final,c(-48,-49)]
-colnames(acess)
-variaveis_am <- c("ID", "Longitude", "Latitude", "N_CAMPO", "Litotipo", "Lote",
-                  "N_LAB", "CLASSE_AM", "Matriz", "Peso_gr", acess$'Código do Método',
-                  "Observação")
+ra_am_sel <- ra_am[6:linha_final,]
+
+variaveis_am <- gsub(" (R$/amostra)", "", variaveis_am, fixed = TRUE)
+variaveis_am <- gsub("-", "_", variaveis_am, fixed = TRUE)
+variaveis_am <- gsub(" (R$/kg)", "", variaveis_am, fixed = TRUE)
+
 colnames(ra_am_sel) <- variaveis_am
-str(ra_am_sel)
+
 ra_am_sel <- transform(ra_am_sel, Latitude = as.numeric(Latitude))
 ra_am_sel <- transform(ra_am_sel, Longitude = as.numeric(Longitude))
-ra_am_sel<- transform(ra_am_sel, PRP102_E...B = as.numeric(PRP102_E...B))
-ra_am_sel <- transform(ra_am_sel, PRP102_Y...B = as.numeric(PRP102_Y...B))
-ra_am_sel <- transform(ra_am_sel, PREPS80P...B = as.numeric( PREPS80P...B))
+ra_am_sel<- transform(ra_am_sel, PRP102_E_B = as.numeric(PRP102_E_B))
+ra_am_sel <- transform(ra_am_sel, PRP102_Y_B = as.numeric(PRP102_Y_B))
+ra_am_sel <- transform(ra_am_sel, PREPS80P_B = as.numeric( PREPS80P_B))
 ra_am_sel <- ra_am_sel[!is.na(ra_am_sel$N_CAMPO),]
 os[[i]] <- ra_am_sel
-} 
-df_os[[1]] = do.call(plyr::rbind.fill, os)
-df_os[[2]] = acess
-names(df_os) <- c("RA_OS", "DOC_LAB")
+}
+df_os = do.call(plyr::rbind.fill, os)
 return(df_os)
 }

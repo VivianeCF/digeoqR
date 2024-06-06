49f2b7b4ef3a0acfa432d44d429005c6a882e344
Viviane Carillo Ferrari <vcarilloferrari@gmail.com>
2024-06-06 14:29:02 -0300

    insere novo modelo

--- R/arruma_boletins.R
+++ R/arruma_boletins.R
@@ -53,4 +53,45 @@ for (i in 1:length(list_bol)) {
               "", quote = FALSE, fileEncoding = "latin1")
 }
 }
+  if(modelo == 2) {
+    ################################################################################
+    ## MODELO 2010 - fosfato
+    ################################################################################
+
+    ## lista dos arquivos dos boletins
+    arquivos <- list.files(dir_in, pattern = "*.csv")
+    list_bol <- paste0(dir_in, arquivos)
+    bol=0
+    df_tudo <- 0
+    for (i in 1:length(list_bol)) {
+      df_tudo = read.csv2(
+        bol <- paste0(dir_in, list.files(dir_in, pattern = "*.csv"))[i],
+        fileEncoding = "latin1", header = FALSE
+      )
+
+      df_adm <- df_tudo[1:10,]
+      df_an <- df[11:nrow(df),]
+      df_s <- df_an %>% tidyr::separate(V1, c("NLAB", "COD"), sep = " ") %>%
+        dplyr::relocate(COD, .before = NLAB)
+      df_s[!is.na(df_s$COD), "NLAB"]<- df_s[!is.na(df_s$COD), "COD"]
+
+      df_s[df_s$V2 == "SEDIMENTO CORRENTE", "COD"] <- "SMP"
+      df_s[df_s$V2 == "REP", "COD"] <- "REP"
+      df_s[df_s$V2 == "STD", "COD"] <- "STD"
+
+      require(magrittr)
+      df_an2 <- df_s %>%
+        tibble::add_column(col1 = NA,
+                           col2 = NA) %>%
+        dplyr::relocate(col1, .before = COD) %>%
+        dplyr::relocate(col2, .after = NLAB)
+      colnames(df_an2) <- paste0("V", 1:ncol(df_an2))
+      arrumado <- dplyr::bind_rows(df_adm, df_an2)
+      colnames(arrumado) <- paste0("V", 1:ncol(arrumado))
+
+      write.table(arrumado, paste0(dir_out, arquivos[i]),
+                  row.names = FALSE, col.names = FALSE, sep = ";", dec = ",", na =
+                    "", quote = FALSE, fileEncoding = "latin1")
+    }
+  }
 }
